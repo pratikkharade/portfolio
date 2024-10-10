ebdf21e3b743a9b29d8c4d97f576b1fadef91338
pratikkharade <53163485+pratikkharade@users.noreply.github.com>
2024-10-10 17:07:33 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ec34a1a26c02996693720f2819321415.js"
+  "/portfolio/precache-manifest.e1a5611826c7bdc0e51b4f231c6fe010.js"
 );
 
 self.addEventListener('message', (event) => {
