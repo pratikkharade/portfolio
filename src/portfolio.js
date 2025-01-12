da9dcf87a56009f0b6833d081ed58def2176f2cc
pratikkharade <53163485+pratikkharade@users.noreply.github.com>
2025-01-11 22:29:03 -0700

    Update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -30,7 +30,7 @@ const greeting = {
     "build seamless applications that empower users to thrive " +
     "in programming. Let’s code a better future!",
   githubProfile: "https://github.com/pratikkharade/",
-  resumeLink: "https://drive.google.com/file/d/1VkfKAqIBRn8S1ZS6MbjBgmAoH0JGpVZt/view?usp=sharing"
+  resumeLink: "https://drive.google.com/file/d/1osNPW7y5FSPpIbtcwqxpUTflTHRbMsrH/"
 };
 
 // Social Media Links
